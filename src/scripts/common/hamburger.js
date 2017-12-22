2bf430b7b183db8eb6b841fb4cf28bda7ac7459e
MaksEra <maks9306@gmail.com>
2017-12-22 23:40:43 +0300

    add hamburger menu and some fixed

--- src/scripts/common/hamburger.js
+++ src/scripts/common/hamburger.js
@@ -1,27 +1,27 @@
-//$(function() {
-//  $('#ham-toggle').on('click', function() {
-//    $(this).toggleClass("on");
-//    $('.hamburger-menu').toggleClass("on");
-//    $('.nav__list').toggleClass('hidden');
-//  });
-//})
+$(function() {
+  $('#ham-toggle').on('click', function() {
+    $(this).toggleClass("on");
+    $('.hamburger-menu').toggleClass("on");
+    $('.nav__list').toggleClass('hidden');
+  });
+})
 
-export default function initHamburger() {
-  let hamburgerLink = document.querySelector(".header__hamburger-menu");
-  if (hamburgerLink) {
-    hamburgerLink.addEventListener("click", e => {
-      e.preventDefault();
-      // добавляем класс или убириаем активный класс у hamburger
-      document.querySelector(".hamburger").classList.toggle("hamburger--plus");
-
+//export default function initHamburger() {
+ // let hamburgerLink = document.querySelector(".header__hamburger-menu");
+ // if (hamburgerLink) {
+ //   hamburgerLink.addEventListener("click", e => {
+ //     e.preventDefault();
+ //     // добавляем класс или убириаем активный класс у hamburger
+ //     document.querySelector(".hamburger").classList.toggle("hamburger--plus");
+//
       // расставляем активные классы на другие связанные элементы
-      document
-        .querySelector(".header__pages-list")
-        .classList.toggle("header__pages-list--visible");
-      document
-        .querySelector(".header__hamburger-menu")
-        .classList.toggle("header__hamburger-menu--z-indexed");
-      document.querySelector("body").classList.toggle("body--overflow-hidden");
-    });
-  }
-}
+ //     document
+  //      .querySelector(".header__pages-list")
+ //       .classList.toggle("header__pages-list--visible");
+  //    document
+  //      .querySelector(".header__hamburger-menu")
+  //      .classList.toggle("header__hamburger-menu--z-indexed");
+  //    document.querySelector("body").classList.toggle("body--overflow-hidden");
+  //  });
+  //}
+//}
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
-//const hamburgerMenu = require('./common/hamburger');
+const hamburgerMenu = require('./common/hamburger');
 const authForm = require('./common/auth-button');
 //const googleMap = require('./common/googleMaps')
 
 authForm();
-//hamburgerMenu();
+hamburgerMenu();
